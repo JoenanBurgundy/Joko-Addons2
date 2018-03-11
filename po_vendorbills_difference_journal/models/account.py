5a55e2985360eb2971b84db4f8d51e1523c68aaf
Joenan Nadhir Rayhan <joenan@Joenans-MacBook.local>
2018-03-11 21:37:19 +0700

    diff_journal invoice bugfix

--- po_vendorbills_difference_journal/models/account.py
+++ po_vendorbills_difference_journal/models/account.py
@@ -18,8 +18,7 @@ class AccountInvoice(models.Model):
     def invoice_line_move_line_get(self):
         product_obj = self.env['product.product']
         res = []
-        total_last_value = 0.0
-        value_to_inv = 0.0
+        total_last_value, value_to_inv = 0.0, 0.0
         for line in self.invoice_line_ids:
             if line.quantity == 0:
                 continue
@@ -37,7 +36,7 @@ class AccountInvoice(models.Model):
                 'name': line.name.split('\n')[0][:64],
                 'price_unit': line.price_unit,
                 'quantity': line.quantity,
-                'price': line.purchase_line_id.last_received_value if line.purchase_line_id.last_received_value \
+                'price': line.purchase_line_id.last_received_value if line.purchase_line_id and line.purchase_line_id.last_received_value \
                     else line.price_subtotal,
                 'account_id': line.account_id.id,
                 'product_id': line.product_id.id,
@@ -51,54 +50,30 @@ class AccountInvoice(models.Model):
                 move_line_dict['analytic_line_ids'] = [(0, 0, line._get_analytic_line())]
             res.append(move_line_dict)
             
-            total_last_value += line.purchase_line_id.last_received_value
-            value_to_inv += line.price_subtotal if line.product_id.type == 'product' else 0
-            line.purchase_line_id.last_received_value = 0
+            if line.purchase_line_id:
+                total_last_value += line.purchase_line_id.last_received_value
+                value_to_inv += line.price_subtotal if line.product_id.type == 'product' else 0
+                line.purchase_line_id.last_received_value = 0
             
         # create write-off line
-        if not self.partner_id.diff_account_receivable or not self.partner_id.diff_account_payable:
-            raise UserError(_('Please configure the default Difference Account on Partner.'))
         diff = total_last_value - value_to_inv
-        move_line_dict = {
-            'type': 'src',
-            'name': 'Price different',
-            'price_unit': diff,
-            'quantity': 1,
-            'price': diff,
-            'account_id': self.partner_id.diff_account_receivable.id if self.type in ('out_invoice', 'out_refund') \
-                else self.partner_id.diff_account_payable.id,
-            'invoice_id': self.id,
-            'analytic_tag_ids': analytic_tag_ids
-        }
-        res.append(move_line_dict)
+        if diff:
+            if not self.partner_id.diff_account_receivable or not self.partner_id.diff_account_payable:
+                raise UserError(_('Please configure the default Difference Account on Partner.'))
+            move_line_dict = {
+                'type': 'src',
+                'name': 'Price different',
+                'price_unit': diff,
+                'quantity': 1,
+                'price': diff,
+                'account_id': self.partner_id.diff_account_receivable.id if self.type in ('out_invoice', 'out_refund') \
+                    else self.partner_id.diff_account_payable.id,
+                'invoice_id': self.id,
+                'analytic_tag_ids': analytic_tag_ids
+            }
+            res.append(move_line_dict)
         return res
     
-#     @api.model
-#     def invoice_line_move_line_get(self):
-#         product_obj = self.env['product.product']
-#         res = super(AccountInvoice, self).invoice_line_move_line_get()
-#         purchase = self.invoice_line_ids[0].purchase_line_id.order_id
-#         value_to_inv = sum([v['price'] if product_obj.browse(v['product_id']).type == 'product' else 0 for v in res])
-#         diff = purchase.last_received_value - value_to_inv
-#         import ipdb;ipdb.set_trace()
-#         move_line_dict = {
-#             'type': 'src',
-#             'name': 'Price Different',
-#             'price_unit': diff,
-#             'quantity': 1,
-#             'price': diff,
-#             'account_id': line.account_id.id,
-#             'product_id': line.product_id.id,
-#             'uom_id': line.uom_id.id,
-#             'account_analytic_id': line.account_analytic_id.id,
-#             'tax_ids': tax_ids,
-#             'invoice_id': self.id,
-#             'analytic_tag_ids': analytic_tag_ids
-#         }
-#         purchase.last_received_value = 0
-#         return res
-
-
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
